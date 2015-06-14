40bf163c41943e581f250ca3bbee2a962304f251
Euan Chow <euanchow@dhcp-128-189-233-199.ubcsecure.wireless.ubc.ca>
2015-06-14 15:31:42 -0700

    AdminQueries update 6.20

--- src/dbQueryLibraries/AdminQueries.java
+++ src/dbQueryLibraries/AdminQueries.java
@@ -1,150 +1,242 @@
 package dbQueryLibraries;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.*;
 
 public class AdminQueries {
 	
 	//6.9
-	public void assignCabinSupervisor(Statement stmt, String cabinID, String counsellor) throws SQLException
+	public void assignCabinSupervisor(Connection con, String cabinID, String counsellor) throws SQLException
 	{
-		ResultSet rs = stmt.executeQuery(
-			"SELECT facility_ID " +
+		PreparedStatement ps = con.prepareStatement(
+			"SELECT fid " +
 			"FROM Cabin " +
-			"WHERE cabin_ID = '" + cabinID + "' " +
-				"AND facility_ID IN (SELECT ca.facility_ID " +
-									"FROM Camp ca, Counsellor co " +
-									"WHERE co.camp_name = ca.name " +
-										"AND co.name = '" + counsellor + "' ");
-	
+			"WHERE id = ? " +
+				"AND fid IN (SELECT ca.fid " +
+							"FROM Camp ca, Counsellor co " +
+							"WHERE co.camp_name = ca.name " +
+								"AND co.name = ?");
+		ps.setString(1, cabinID);
+		ps.setString(2, counsellor);
+		
+		ResultSet rs = ps.executeQuery();
+		ps.clearBatch();
+		ps.clearParameters();
+		
 		if (!rs.isBeforeFirst() ) {    
-			 System.out.println("No data.");
 			 System.out.println("Error - Counsellor not at cabin's facility");
 		} 
 		else {
-			int rowCount = stmt.executeUpdate(
+			ps = con.prepareStatement(
 				"UPDATE Counsellor " + 
-				"SET cabin_id = '" + cabinID + "' " +
-				"WHERE name = " + counsellor + "'");
+				"SET cabin_id = ? " +
+				"WHERE name = ?");
+			ps.setString(1, cabinID);
+			ps.setString(2, counsellor);
+			int rowCount = ps.executeUpdate();
 			
 			System.out.println("Completed, " + rowCount + "lines updated.");
 		}
+		
+		ps.close();
 	}
 	
 	//6.10
-	public void setWorksAt(Statement stmt, int instructorID, String camp_name) throws SQLException
+	public void setWorksAt(Connection con, int instructorID, String camp_name) throws SQLException
 	{
-		ResultSet rs = stmt.executeQuery(
+		PreparedStatement ps = con.prepareStatement(
 			"SELECT camp_name "+
 			"FROM Counsellor " +
-			"WHERE id = '" + instructorID + "'");
+			"WHERE id = ?");
+		ps.setInt(1, instructorID);
+		
+		ResultSet rs = ps.executeQuery();
+		ps.clearBatch();
+		ps.clearParameters();
 		
 		String existCamp = rs.getString("camp_name");
 		if(!rs.wasNull() ) {
 			 System.out.println("Error - Counsellor already works at " + existCamp + ".");
 		}
 		else {
-			int rowCount = stmt.executeUpdate(
+			ps = con.prepareStatement(
 				"UPDATE Counsellor " + 
-				"SET camp_name = '" + camp_name + "' " +
-				"WHERE id = " + instructorID + "'");
-				
+				"SET camp_name = ? " +
+				"WHERE id = ?");
+			ps.setString(1, camp_name);
+			ps.setInt(2, instructorID);
+			int rowCount = ps.executeUpdate();
+			
 			System.out.println("Completed, " + rowCount + "lines updated.");
 		}
+		
+		ps.close();
 	}
 	
 	//6.11
-	public void assignRegToCounsellor(Statement stmt, int instructorID, int confirmNo) throws SQLException
+	public void assignRegToCounsellor(Connection con, int instructorID, int confirmNo) throws SQLException
 	{
-		ResultSet rs = stmt.executeQuery(
-			"SELECT R.confirmation#, R.camp_name, R.counsellor_ID, C.counsellor_ID " +
+		PreparedStatement ps = con.prepareStatement(		
+			"SELECT R.conf_num, R.camp_name, R.counsellor_id, C.id " +
 			"FROM Counsellor C, Registration R " +
 			"WHERE R.camp_name = C.camp_name " + 
-				"AND R.confirmation# = '" + confirmNo + "'");
+				"AND R.conf_num = ?");
+		ps.setInt(1, confirmNo);
 		
+		ResultSet rs = ps.executeQuery();
+		ps.clearBatch();
+		ps.clearParameters();		
+		
+		String output = null;
 		while (rs.next()) {
 			int ID = rs.getInt(3);
 			if(!rs.wasNull()) {
-				System.out.println("Error - Registration already has assigned counsellor.");
+				output = "Error - Registration already has assigned counsellor.";
 				break;
 			}
 			else if(rs.getInt(4) == instructorID) {
-				int rowCount = stmt.executeUpdate(
+				ps = con.prepareStatement(
 					"UPDATE Registration " + 
-					"SET counsellor_ID = '" + instructorID + "' " +
-					"WHERE confirmation# = " + confirmNo + "'");
-					
-				System.out.println("Completed, " + rowCount + "lines updated.");
+					"SET counsellor_id = ? " +
+					"WHERE conf_num = ?");
+					ps.setInt(1, instructorID);
+					ps.setInt(2, confirmNo);
+				int rowCount = ps.executeUpdate();
+				
+				output = "Completed, " + rowCount + "lines updated.";
 				break;
 			}
 		}
 		
-		System.out.println("Error - Counsellor does not work at specified camp.");
+		if (output == null){
+			output = "Error - Counsellor does not work at specified camp.";
+		}
+		System.out.println(output);
+		ps.close();
 	}
 	
 	//6.13
-	public String checkRegPayments(Statement stmt, String camp_name) throws SQLException
+	public ArrayList<String> checkRegPayments(Connection con, String camp_name) throws SQLException
 	{
-		ResultSet rs = stmt.executeQuery(
-			"SELECT C.phone# " +
+		PreparedStatement ps = con.prepareStatement(
+			"SELECT C.phone_num " +
 			"FROM Camper C, Registration R " +
-			"WHERE C.camper_id = R.camper_ID " +
-				"AND R.camp_name = '" + camp_name + "' " +
+			"WHERE C.id = R.camper_ID " +
+				"AND R.camp_name = ? " +
 				"AND R.is_paid IS FALSE");
-		
-		StringBuilder output = new StringBuilder();
+		ps.setString(1, camp_name);
+		ResultSet rs = ps.executeQuery();
+
+		ArrayList<String> output = new ArrayList<String>();
 		
 		if (!rs.isBeforeFirst() ) {    
-			output.append("All paid");
+			System.out.println("All paid.");
 		} 
 		else {
 			while (rs.next()) {
-				output.append(rs.getString(1) + "\n");
+				output.add(rs.getString(1));
 			}
 		}
 		
-		return output.toString();
+		ps.close();
+		return output;
 	}
 	
 	//6.14
-	public String multipleCamps(Statement stmt, int[] camperID, String[] camp_name) throws SQLException
+	public ArrayList<String> multipleCamps(Connection con, ArrayList<Integer> camperID, ArrayList<String> camp_name) throws SQLException
 	{
+		Statement stmt = con.createStatement();
 		StringBuilder view = new StringBuilder("CREATE VIEW campersFilter AS (SELECT * FROM Registration WHERE ");
 		int i;
 		
-		for (i = 0; i < camperID.length ; i++) {
-			if (i == (camperID.length - 1) )
-				view.append("camper_ID = '" + camperID[i] + "')");
+		for (i = 0; i < camperID.size() ; i++) {
+			if (i == (camperID.size() - 1) )
+				view.append("camper_id = '" + Integer.toString(camperID.get(i)) + "')");
 			else 
-				view.append("camper_ID = '" + camperID[i] + "' OR ");
+				view.append("camper_id = '" + Integer.toString(camperID.get(i)) + "' OR ");
 		}
 		
 		stmt.executeUpdate(view.toString());
 		
 		StringBuilder query = new StringBuilder("SELECT camper_ID, COUNT(*) FROM campersFilter WHERE ");
 		
-		for (i = 0; i < camp_name.length ; i++) {
-			if (i == (camp_name.length - 1) )
-				query.append("camper_ID = '" + camp_name[i] + "' ");
+		for (i = 0; i < camp_name.size() ; i++) {
+			if (i == (camp_name.size() - 1) )
+				query.append("camp_name = '" + camp_name.get(i) + "' ");
 			else 
-				query.append("camper_ID = '" + camp_name[i] + "' OR ");
+				query.append("camp_name = '" + camp_name.get(i) + "' OR ");
 		}
 		
-		query.append("GROUP BY camper_ID HAVING COUNT(*) > 1");
+		query.append("GROUP BY camper_id HAVING COUNT(*) > 1");
 		ResultSet rs = stmt.executeQuery(query.toString());
 		
-		StringBuilder output = new StringBuilder();
+		ArrayList<String> output = new ArrayList<String>();
 		
 		if (!rs.isBeforeFirst() ) {    
-			output.append("None");
+			System.out.println("None");
 		} 
 		else {
 			while (rs.next()) {
-				output.append(rs.getString(1) + "\n");
+				output.add(rs.getString(1));
 			}
 		}
 		
-		return output.toString();
+		stmt.executeUpdate("DROP VIEW campersFilter");
+		stmt.close();
+		return output;
+	}
+	
+	//6.15
+	public ArrayList<String> checkCounsellor(Connection con) throws SQLException
+	{
+		Statement stmt = con.createStatement();
+		ResultSet rs = stmt.executeQuery(
+			"SELECT * " + 
+			"FROM Counsellor " +
+			"WHERE camp_name IS NULL");
+		
+		ArrayList<String> output = new ArrayList<String>();
+		
+		if (!rs.isBeforeFirst() ) {    
+			System.out.println("None");
+		} 
+		else {
+			while (rs.next()) {
+				output.add(rs.getString(1));
+			}
+		}
+		
+		stmt.close();
+		return output;
+	}
+	
+	//6.20
+	public ArrayList<String> multipleSessions(Connection con) throws SQLException
+	{
+		Statement stmt = con.createStatement();
+		stmt.executeUpdate(
+			"CREATE VIEW campersFilter AS (SELECT R.camper_id, C.name, R.sid " +
+			"FROM Registration R, Camper C " + 
+			"GROUP BY camper_id " +
+			"HAVING COUNT(*) > 1)");
+		
+		ResultSet rs = stmt.executeQuery(
+			"SELECT DISTINCT R.camper_id, C.name " +
+			"FROM campersFilter" );
+		
+		ArrayList<String> output = new ArrayList<String>();
+		
+		if (!rs.isBeforeFirst() ) {    
+			System.out.println("None");
+		} 
+		else {
+			while (rs.next()) {
+				output.add(rs.getString(1) + " - " + rs.getString(2));
+			}
+		}
+		
+		stmt.executeUpdate("DROP VIEW campersFilter");
+		stmt.close();
+		return output;
 	}
 }
